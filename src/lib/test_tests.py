3f9abb1e5f800c9c0d46c112d8686c9ddf9e7523
Matthew Lancellotti <mareoraft@gmail.com>
2018-02-11 16:50:11 -0500

    tests

--- src/lib/test_tests.py
+++ src/lib/test_tests.py
@@ -6,6 +6,29 @@ from config import *
 from lib.stream import stream
 
 
-def test_tests():
+def test_1():
+	stream(PATH('test', 1))
+
+def test_2():
+	stream(PATH('test', 2))
+
+def test_3():
+	stream(PATH('test', 3))
+
+def test_4():
+	stream(PATH('test', 4))
+
+def test_5():
+	stream(PATH('test', 5))
+
+def test_6():
+	stream(PATH('test', 6))
+
+def test_7():
+	stream(PATH('test', 7))
+
+def test_8():
 	stream(PATH('test', 8))
 
+def test_9():
+	stream(PATH('test', 9))
